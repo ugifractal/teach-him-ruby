d62f62696c75ad4391d837dd0093d03e8735af28
ugifractal <ugidmtest@gmail.com>
2023-11-30 18:41:43 +0700

    Lesson 1

---
+++ practice_1_penjumlahan.rb
@@ -0,0 +1,7 @@
+# Modifikasi method ```hitung_jumlah``` sehingga menghasilkan nilai yang sesuai
+def hitung_jumlah(array)
+  0
+end
+
+hasil = hitung_jumlah([8, 9, 10, 12, 19, 34, 56, 67, 123, 106])
+puts("Hasil penjumlahan= #{hasil}")
