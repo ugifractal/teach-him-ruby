165175d8815464e1ac49d59b932528cc7ed4df54
tsaqib29 <113277896+tsaqib29@users.noreply.github.com>
2023-12-02 10:32:49 +0700

    Update practice_1_penjumlahan.rb
    hh

--- practice_1_penjumlahan.rb
+++ practice_1_penjumlahan.rb
@@ -1,6 +1,6 @@
 # Modifikasi method ```hitung_jumlah``` sehingga menghasilkan nilai yang sesuai
 def hitung_jumlah(array)
-  0
+  array.sum
 end
 
 hasil = hitung_jumlah([8, 9, 10, 12, 19, 34, 56, 67, 123, 106])
