cdbf12f05e70ffa084f8e748b3b790c068bb9cff
ugifractal <ugidmtest@gmail.com>
2023-12-03 08:40:26 +0700

    Add second practice

---
+++ practice_2_ikut_pemilu.rb
@@ -0,0 +1,18 @@
+semua_warga = [
+  { nama: 'Ujang', usia: 23, profesi: 'guru' },
+  { nama: 'Beni', usia: 17, profesi: 'pedagang' },
+  { nama: 'Wawan', usia: 18, profesi: 'tentara' },
+  { nama: 'Yani', usia: 20, profesi: 'buruh' },
+  { nama: 'Tomas', usia: 16, profesi: 'pelajar' }
+]
+
+# Modifikasi method ini, sehingga menghasilkan jumlah peserta pemilu yang sesuai.
+# Input: array dari warga
+# Output: jumlah warga dengan usia lebih atau sama dengan 1t7 tahun dan bukan seorang tentara
+def jumlah_warga_dengan_hak_pilih(warga)
+  0
+end
+
+# Hasil seharusnya 3, yaitu Ujang, Beni dan Yani
+puts "Jumlah Peserta pemilu: #{jumlah_warga_dengan_hak_pilih(semua_warga)} orang"
+
